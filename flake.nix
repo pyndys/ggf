c01523a8eb6d6ecf63718ca69eb3deac7d0991db
pyndys <mikumiku@mail>
2026-05-10 11:51:28 +0300

    Create flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Great Go Fetch (ggf) - a fast system fetch tool written in Go";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.buildGoModule {
+        pname = "ggf";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = null;
+      };
+    });
+
+    ## For nix run
+    apps = forAllSystems (pkgs: {
+      default = {
+        type = "app";
+        program = "${self.packages.${pkgs.system}.default}/bin/ggf";
+      };
+    });
+  };
+}
